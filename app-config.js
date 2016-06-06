bcfb7c561c5d53e2dd9a5df8ae36e04a63f0d1b8
luanhaipeng <luanhaipeng@qihangedu.com>
2016-06-06 17:58:04 +0800

    修改了一些bug

--- app-config.js
+++ app-config.js
@@ -1,8 +1,18 @@
+function getEnvironment() {
+
+    if(process.env.NODE_ENV =="production"){
+        return "release";
+    }else {
+        return "development";
+    }
+}
+var _ENVIRONMENT = getEnvironment();
+
+console.log("_ENVIRONMENT::::"+_ENVIRONMENT);
 
 module.exports = {
     SERVE_PORT:3000,
-    //_ENVIRONMENT:"development", //release
-    _ENVIRONMENT:"release", //release
+    _ENVIRONMENT:_ENVIRONMENT,
     MONGOOSE_CONNECTION:"mongodb://localhost:27017/helloworld",
     ROOT_DIR :__dirname,
     DEFAULT_AVATAR :"/public/static/images/avatar0.png"
